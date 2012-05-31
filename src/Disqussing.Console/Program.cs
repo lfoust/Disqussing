02ae0e959a13b9927f3806c7b8110e31aafb223d
Luke Foust <luke@foust.com>
2012-05-31 14:07:18 -0700

    Updated test client to show use with IOC container

--- src/Disqussing.Console/Program.cs
+++ src/Disqussing.Console/Program.cs
@@ -1,20 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Disqussing
+﻿namespace Disqussing
 {
+	using System;
+	using Ninject;
+	using Ninject.Modules;
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			DisqusClient client = new DisqusClient(new PrivateApiKey(""), "3.0", new UrlClient());
-			var user = client.GetUser("lfoust");
-			Console.WriteLine(user.Name);
+			IKernel kernel = new StandardKernel(new DisqusModule());
+			var program = kernel.Get<Shell>();
+			program.Start();
+
 			Console.WriteLine("Done.");
 			Console.ReadLine();
+		}
+	}
+
+	public class DisqusModule : NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<IUrlClient>().To<UrlClient>();
+			Bind<IApiKey>().To<PrivateApiKey>().WithConstructorArgument("key", "");
+			Bind<DisqusClient>().To<DisqusClient>().WithConstructorArgument("version", "3.0");
+			Bind<IShell>().To<Shell>();
+		}
+	}
+
+	public interface IShell
+	{
+		void Start();
+	}
+
+	public class Shell : IShell
+	{
+		private DisqusClient Client { get; set; }
+
+		public Shell(DisqusClient client)
+		{
+			Client = client;
+		}
+
+		public void Start()
+		{
+			var user = Client.GetUser("lfoust");
+			Console.WriteLine(user.Name);
 		}
 	}
 }
